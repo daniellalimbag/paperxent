56107412ce52aee93a38aba35db699250ec03544
Dani Limbag <limbagdaniella@gmail.com>
2026-06-09 22:59:41 +0800

    feat(shared-types): define paginated response and transaction DTOs

--- apps/api/src/modules/index.ts
+++ apps/api/src/modules/index.ts
@@ -3,6 +3,7 @@ import authRouter from './auth/auth.routes.js';
 import { marketRouter } from './market/market.routes.js';
 import { portfoliosRouter } from './portfolios/portfolios.routes.js';
 import { tradesRouter } from './trades/trades.routes.js';
+import { transactionsRouter } from './transactions/transactions.routes.js';
 import { usersRouter } from './users/users.routes.js';
 
 export const apiRouter = Router();
@@ -10,5 +11,6 @@ export const apiRouter = Router();
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/trade', tradesRouter);
+apiRouter.use('/transactions', transactionsRouter);
 apiRouter.use('/portfolio', portfoliosRouter);
 apiRouter.use('/market', marketRouter);
